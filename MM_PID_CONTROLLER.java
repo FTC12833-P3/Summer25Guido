6101b4b99afc1d8c083584266e11c4604beeee49
lincoln12833 <Lincolnbruning@protonmail.com>
2025-05-21 12:31:34 -0400

    add MM_PID_CONTROLLER class (not tested and no I)

---
+++ MM_PID_CONTROLLER.java
@@ -0,0 +1,28 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+public class MM_PID_CONTROLLER {
+    private final ElapsedTime dTime = new ElapsedTime();
+
+    private final double P_CO_EFF;
+    //private final double I_CO_EFF;
+    private final double D_CO_EFF;
+
+    private double prevError = 0;
+
+    public MM_PID_CONTROLLER(double P_CO_EFF, double I_CO_EFF, double D_CO_EFF){
+        this.P_CO_EFF = P_CO_EFF;
+        //this.I_CO_EFF = I_CO_EFF;
+        this.D_CO_EFF = D_CO_EFF;
+    }
+
+    public double getPID(double error){
+        double P = error * P_CO_EFF;
+        double I = 0; //no I for now
+        double D = (error - prevError) / dTime.milliseconds() * D_CO_EFF;
+        dTime.reset();
+
+        return P + I + D;
+    }
+}
